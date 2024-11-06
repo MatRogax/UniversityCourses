1431932ebf8391411e4f887d7b519832f692e554
matheus rogato <matheusrogato@192.168.0.8>
2024-11-06 18:12:07 -0300

    chore: mudança na depêndencia de estilização

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,5 +2,6 @@
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
+//= link application.tailwind.css
 //= require materialize
 //= link_tree ../builds
